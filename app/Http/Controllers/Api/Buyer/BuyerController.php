9ca447eab985293fc39a43a2509c363e3626e8da
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-18 19:29:42 -0300

    Index method has been implemented

--- app/Http/Controllers/Api/Buyer/BuyerController.php
+++ app/Http/Controllers/Api/Buyer/BuyerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\Buyer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Buyer;
 
 class BuyerController extends Controller
 {
@@ -14,28 +14,14 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $buyers = Buyer::has('transactions')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()
+            ->json([
+                'success' => true,
+                'message' => 'All buyers',
+                'data' => $buyers
+            ], 200);
     }
 
     /**
@@ -45,40 +31,6 @@ class BuyerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
