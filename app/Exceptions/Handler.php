96a66750510c60d26ed8e68ddb0a49c8d0b9fdd2
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-18 22:26:45 -0300

    Now all exceptions is returning a json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,14 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponse;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -52,7 +57,38 @@ class Handler extends ExceptionHandler
             return $this->errorResponse($resource . ' not found.', 404);
         }
 
-        return parent::render($request, $e);
+        if ($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $e);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return $this->errorResponse('User is not authorized.', 403);
+        }
+
+        if ($e instanceof NotFoundHttpException) {
+            return $this->errorResponse('Route not found.', 404);
+        }
+
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('Invalid request method .', 405);
+        }
+
+        if ($e instanceof HttpException) {
+            $message = $e->getMessage();
+            $status = $e->getStatusCode();
+            return $this->errorResponse($message, $status);
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $e);
+        }
+
+        return $this->errorResponse('An internal error occurred. Please try again later.', 500);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('User is not authenticated.', 401);
     }
 
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
