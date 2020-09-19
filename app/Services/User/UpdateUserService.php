af02ef5fb4baed79a5376e0a8c96ab976dd6bf22
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-19 11:55:03 -0300

    Update user service has been refactored

--- app/Services/User/UpdateUserService.php
+++ app/Services/User/UpdateUserService.php
@@ -23,39 +23,78 @@ class UpdateUserService extends AbstractService
         
         $user = User::findOrFail($dto->id);
 
-        if ($dto->name) {
-            $user->name = $dto->name;
-        }
-
-        if ($dto->email && $user->email != $dto->email) {
-            $tempUser = User::where('email', $dto->email)->first();
-
-            if (!$tempUser) {
-                $user->verified = User::UNVERIFIED_USER;
-                $user->verification_token = User::verificationTokenGenerator();
-                $user->email = $dto->email;
-            } else {
-                throw new AppError('An user with this email already exists', 409);
-            }
-        }
-
-        if ($dto->password) {
-            $user->password = bcrypt($dto->password);
-        }
-
-        if ($dto->admin) {
-            if (!$user->isVerified()) {
-                throw new AppError('User must be verified to change admin value.', 409);
-            }
-
-            $user->admin = $dto->admin;
-        }
+        $user = $this->handleChanges($dto, $user);
 
         if (!$user->isDirty()) {
             throw new AppError('At least one value must be modified to update the user.', 409);
         }
 
         $user->save();
+
+        return $user;
+    }
+
+    private function handleChanges(UpdateUserDto $dto, User $user) : User
+    {
+        $user = $this->theNameHasChanged($dto, $user);
+
+        $user = $this->theEmailHasChanged($dto, $user);
+
+        $user = $this->thePasswordHasChanged($dto, $user);
+
+        $user = $this->theAdminHasChanged($dto, $user);
+
+        return $user;
+    }
+
+    private function theNameHasChanged(UpdateUserDto $dto, User $user) : User
+    {
+        if ($dto->name) {
+            $user->name = $dto->name;
+        }
+
+        return $user;
+    }
+
+    private function theEmailHasChanged(UpdateUserDto $dto, User $user) : User
+    {
+        if (!$dto->email || $user->email == $dto->email) {
+            return $user;
+        }
+
+        $tempUser = User::where('email', $dto->email)->first();
+
+        if ($tempUser) {
+            throw new AppError('An user with this email already exists', 409);
+        }
+
+        $user->verified = User::UNVERIFIED_USER;
+        $user->verification_token = User::verificationTokenGenerator();
+        $user->email = $dto->email;
+
+        return $user;
+    }
+
+    private function thePasswordHasChanged(UpdateUserDto $dto, User $user) : User
+    {
+        if ($dto->password) {
+            $user->password = bcrypt($dto->password);
+        }
+
+        return $user;
+    }
+
+    private function theAdminHasChanged(UpdateUserDto $dto, User $user) : User
+    {
+        if (!$dto->admin) {
+            return $user;
+        }
+
+        if (!$user->isVerified()) {
+            throw new AppError('User must be verified to change admin value.', 409);
+        }
+
+        $user->admin = $dto->admin;
 
         return $user;
     }
