07681dc902420862c3ea710bfba5628bad82c8cd
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-19 21:56:51 -0300

    Sellers products endpoint has been implemented

---
+++ app/Http/Controllers/Api/Seller/SellerProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api\Seller;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+        return $this->collectionResponse('All products', $products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller)
+    {
+        //
+    }
+}
