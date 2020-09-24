55592397d294d77acad90366b884e7994dc4b3ca
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-24 13:58:20 -0300

    Seller resource has been implemented

--- app/Http/Controllers/Api/Seller/SellerController.php
+++ app/Http/Controllers/Api/Seller/SellerController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers\Api\Seller;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Resources\SellerResource;
 use App\Models\Seller;
+use App\Traits\CollectionListHelpers;
+use Illuminate\Http\Request;
 
 class SellerController extends ApiController
 {
+    use CollectionListHelpers;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $sellers = Seller::has('products')->get();
+        $sellers = $this->sortedFilteredAndPaginatedCollection(Seller::has('products')->get(), $request, ['created_at'], ['admin'], SellerResource::class);
         return $this->collectionResponse('All sellers', $sellers);
     }
 
@@ -26,6 +31,6 @@ class SellerController extends ApiController
      */
     public function show(Seller $seller)
     {
-        return $this->resourceResponse('All seller', $seller, 200);
+        return $this->resourceResponse('All seller', new SellerResource($seller), 200);
     }
 }
---
+++ app/Http/Resources/SellerResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SellerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'admin' => $this->admin,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('sellers.show', $this->id)
+                ]
+            ]
+        ];
+    }
+}
