74097c16f8503df4059bfb69f56cb2d73c21557e
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-24 23:05:18 -0300

    Buyer resource has been refactored

--- app/Http/Resources/BuyerResource.php
+++ app/Http/Resources/BuyerResource.php
@@ -25,6 +25,22 @@ class BuyerResource extends JsonResource
                 [
                     'rel' => 'self',
                     'href' => route('buyers.show', $this->id)
+                ],
+                [
+                    'rel' => 'buyer.categories',
+                    'href' => route('buyers.categories.index', $this->id)
+                ],
+                [
+                    'rel' => 'buyer.products',
+                    'href' => route('buyers.products.index', $this->id)
+                ],
+                [
+                    'rel' => 'buyer.sellers',
+                    'href' => route('buyers.sellers.index', $this->id)
+                ],
+                [
+                    'rel' => 'buyer.transactions',
+                    'href' => route('buyers.transactions.index', $this->id)
                 ]
             ]
         ];
