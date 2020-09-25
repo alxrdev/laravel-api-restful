5c6c587a1357762509994fedc1a069ba32122f83
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-24 23:17:32 -0300

    Collection class has been changed

--- app/Traits/CollectionListHelpers.php
+++ app/Traits/CollectionListHelpers.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 
